41f817b8545cec88206dc5c4b7684b096cc9a47e
christopher <christopher@procedural.build>
2020-05-05 11:49:47 +0200

    rename follow functions, add browse categories

--- follow-channel.js
+++ follow-channel.js
@@ -1,34 +1,32 @@
 const AWS = require("aws-sdk");
-const documentClient = new AWS.DynamoDB.DocumentClient();
+const ddb = new AWS.DynamoDB;
 let ret = {};
 
 exports.handler = (event, context, callback) => {
   const { userID, follow_channel_name } = JSON.parse(event.body);
-  
+
   const params = {
     TableName: "users",
-    Key: { "userUUID": userID },
-    UpdateExpression : "set #fc = list_append(#fc, :followed_channels) ",
-    ExpressionAttributeNames : { 
-      "#fc" : "followed_channels"
-    },
-    ExpressionAttributeValues : { 
-        ":followed_channels" : [follow_channel_name],
+    Key: { userUUID: { S : userID } },
+    UpdateExpression : "ADD followed_channels :followed_channel_name",
+    ExpressionAttributeValues : {
+        ":followed_channel_name" : {
+          "SS" : [follow_channel_name]
+        }
     }
   }
-  
-  documentClient.update(params, function(err, data) {
+
+  ddb.updateItem(params, function(err, data) {
     if (err){
       console.log(err, err.stack);
       ret.ddbUpdate = err;
       callback(null, {success: false, message: ["ddb upsert failed"], payload: ret});
-    } 
+    }
     else{
       console.log(data);
       ret.ddbUpdate = data;
       callback(null, {success: true, message: ["ddb upsert succeeded"], payload: ret});
- 
-    } 
+    }
   })
 
 };
